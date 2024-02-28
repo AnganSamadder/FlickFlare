79b8ce306a39ed63ec95579ac1c50fd10e73fcd1
Cole-Wright4195 <ctw39353@uga.edu>
2024-02-27 22:23:19 -0500

    fixed order confirmation

--- frontend/src/app/[movie]/(booking)/order/confirmation/page.tsx
+++ frontend/src/app/[movie]/(booking)/order/confirmation/page.tsx
@@ -1,119 +1,69 @@
 export default function Confirmation() {
   return (
-    <div className="w-[1440px] h-[1024px] relative bg-white">
-      <div className="w-[1313px] h-[758px] left-[88px] top-[141px] absolute">
-        <div className="w-[1313px] h-[718px] left-0 top-[40px] absolute bg-zinc-700 bg-opacity-50 rounded-[17px]" />
-        <div className="w-[952px] h-[351px] left-[301px] top-0 absolute">
-          <div className="w-[952px] h-[281px] left-0 top-[70px] absolute">
-            <div className="left-[104px] top-[24px] absolute text-center">
-              <span className="text-white text-[32px] font-bold font-['Maven Pro'] leading-normal">
-                Order confirmed.  {" "}
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="bg-zinc-700 bg-opacity-50 rounded-[17px] p-6 w-3/4">
+          <div className="text-center mb-8">
+            <h1 className="text-orange-500 text-5xl font-bold font-['Maven Pro'] leading-normal">
+              Order Confirmation
+            </h1>
+            <p className="text-white text-[32px] justify-items-center font-medium font-['Maven Pro']">
+              Order confirmed. View “Order History” to see your current orders.
+              A confirmation email should be sent to your inbox. See you at the theater!
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <div className="mb-4">
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Movie:
               </span>
-              <span className="text-white text-[32px] font-medium font-['Maven Pro'] leading-normal">
-                  View “Order History” to see your current orders.   A
-                confirmation email should be sent to your inbox.  <br />  See
-                you at the theater!
+                <span className="text-white text-2xl font-medium font-['Maven Pro'] ml-2">
+                Iron Man
               </span>
+              </div>
+              <div className="mb-4">
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Showtime:
+              </span>
+                <span className="text-white text-2xl font-medium font-['Maven Pro'] ml-2">
+                10/10, 1:00 PM
+              </span>
+              </div>
+              <div className="mb-4">
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Seats:
+              </span>
+                <span className="text-white text-2xl font-medium font-['Maven Pro'] ml-2">
+                Total: 4 (A3, C4, C5, C6)
+              </span>
+              </div>
+              <div className="mb-4">
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Total Price:
+              </span>
+                <span className="text-white text-2xl font-medium font-['Maven Pro'] ml-2">
+                $32.00
+              </span>
+              </div>
+              <div className="mb-4">
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Order Date:
+              </span>
+                <span className="text-white text-2xl font-normal font-['Maven Pro'] ml-2">
+                10/09/2024, 11:21 AM
+              </span>
+              </div>
+              <div>
+              <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro']">
+                Card:
+              </span>
+                <span className="text-white text-2xl font-normal font-['Maven Pro'] ml-2">
+                ********XXX
+              </span>
+              </div>
             </div>
           </div>
-          <div className="w-[440px] left-[219px] top-0 absolute text-orange-500 text-5xl font-bold font-['Maven Pro'] leading-normal">
-            Order Confirmation
-          </div>
-        </div>
-        <div className="w-[254px] h-[669px] left-[29px] top-[70px] absolute">
-          <div className="w-[200px] left-0 top-0 absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Movie:  {" "}
-            </span>
-            <span className="text-white text-2xl font-medium font-['Maven Pro'] leading-normal">
-              Iron Man
-            </span>
-          </div>
-          <div className="w-[200px] left-0 top-[112px] absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Showtime:  {" "}
-            </span>
-            <span className="text-white text-2xl font-medium font-['Maven Pro'] leading-normal">
-              10/10, 1:00 PM
-            </span>
-          </div>
-          <div className="w-[200px] left-0 top-[209px] absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Seats:  {" "}
-            </span>
-            <span className="text-white text-2xl font-medium font-['Maven Pro'] leading-normal">
-              Total: 4<br />
-              <br />
-              (A3, C4, C5, C6)
-            </span>
-          </div>
-          <div className="w-[200px] left-0 top-[369px] absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Total Price:  {" "}
-            </span>
-            <span className="text-white text-2xl font-medium font-['Maven Pro'] leading-normal">
-              $32.00
-            </span>
-          </div>
-          <div className="w-[254px] left-0 top-[481px] absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Order Date:
-              <br /> {" "}
-            </span>
-            <span className="text-white text-2xl font-normal font-['Maven Pro'] leading-normal">
-              10/09/2024, 11:21 AM
-            </span>
-          </div>
-          <div className="w-[237px] left-0 top-[593px] absolute">
-            <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
-              Card:
-              <br /> {" "}
-            </span>
-            <span className="text-white text-2xl font-normal font-['Maven Pro'] leading-normal">
-              ********XXX
-            </span>
-          </div>
         </div>
       </div>
-      <div className="w-[1470px] h-20 left-0 top-[944px] absolute">
-        <div className="w-[1470px] h-20 left-0 top-0 absolute bg-zinc-700 border border-black" />
-        <div className="w-[583px] h-[25.14px] left-[14px] top-[27.43px] absolute text-orange-500 text-xl font-bold font-['Inter']">
-          © 2024 Cinema E-Booking. All rights reserved.
-        </div>
-      </div>
-      <div className="w-[1440px] h-[95px] left-0 top-0 absolute">
-        <div className="w-[1440px] h-[84px] left-0 top-0 absolute bg-zinc-800 rounded-bl-[20px] rounded-br-[20px] shadow border-black" />
-        <div className="w-[60px] h-[60px] left-[1341px] top-[13px] absolute bg-zinc-800 bg-opacity-0" />
-        <div className="w-[751px] h-[83px] left-[281px] top-[12px] absolute">
-          <div className="w-[751px] h-[63px] left-0 top-0 absolute bg-stone-900 rounded-[29px]" />
-          <div className="w-36 h-[27px] left-[596px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Coming Soon
-          </div>
-          <div className="w-[35px] h-[0px] left-[575px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[175px] h-[30px] left-[382px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Currently Running
-          </div>
-          <div className="w-[35px] h-[0px] left-[361px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[154px] h-[63px] left-[224px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Order History
-          </div>
-          <div className="w-[35px] h-[0px] left-[202px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[97px] h-[63px] left-[130px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Profile
-          </div>
-          <div className="w-[35px] h-[0px] left-[109px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[94px] h-[63px] left-[36px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Home
-          </div>
-        </div>
-        <div className="w-[703px] h-[39px] left-[32px] top-[24px] absolute text-orange-500 text-[40px] font-extrabold font-['Maven Pro'] leading-normal">
-          NinjaFlix
-        </div>
-        <img
-          className="w-[54px] h-[49px] left-[200px] top-[14px] absolute"
-          src="https://via.placeholder.com/54x49"
-        />
-      </div>
-    </div>
   );
 }
