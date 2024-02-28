467a3f69948fdc0fce7fedb054a15bdcc8ebaa8e
Cole-Wright4195 <ctw39353@uga.edu>
2024-02-28 01:59:59 -0500

    fixed login centering i think

--- frontend/src/app/(user)/login/page.tsx
+++ frontend/src/app/(user)/login/page.tsx
@@ -5,10 +5,10 @@ import InputField from "@/app/components/InputField";
 export default function Login({}) {
   return (
     <div className="w-[1440px] h-[1024px] align-top relative bg-black rounded-br-[19px]">
-      <div className="left-[658px] top-[100px] absolute text-orange-500 text-5xl font-bold font-['Maven Pro'] leading-normal">
+      <div className="left-[1085px] top-[100px] absolute text-orange-500 text-5xl font-bold font-['Maven Pro'] leading-normal">
         Login
       </div>
-      <div className="  left-[337px] top-[180px] p-10 absolute">
+      <div className="left-[765px] top-[280px] p-10 absolute">
         <div className="w-[767px] h-[560px] left-0 top-0 absolute bg-zinc-700 bg-opacity-50 rounded-[17px]" />
         <div className="w-[321px] h-[223px] left-[228px] top-[116px] absolute">
           <div className="w-80 h-[76.43px] left-0 top-[115px] absolute">
@@ -51,7 +51,7 @@ export default function Login({}) {
           </button>
         </div>
       </div>
-      <div className="w-[340px] left-[567px] top-[780px] absolute">
+      <div className="w-[340px] left-[980px] top-[890px] absolute">
         <span className="text-orange-50 text-2xl font-medium font-['Maven Pro'] leading-normal">
           Don’t have a account?{" "}
         </span>
