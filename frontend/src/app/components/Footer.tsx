36d004dd9923c5a358d3e239cedadec49ce3f0ca
Angan Samadder <rio.angan@gmail.com>
2024-02-28 03:15:40 -0500

    fix glitch in footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-zinc-700 fixed w-full z-50">
+    <footer className="bg-zinc-700 w-full z-50">
       <div className="py-4 text-orange-500 text-xl font-bold font-['Inter']">
         <p>© 2024 Cinema E-Booking. All rights reserved.</p>
       </div>
