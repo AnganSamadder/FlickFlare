505e378357ed5e878bfcdd59493224a3a5707527
Shahrima Ishrat <si01237@uga.edu>
2024-03-17 23:39:47 -0400

    Start working on removing hardcode
    Start working on Movie page's UI in trying to replace the hardcode

--- frontend/src/app/[movie]/page.tsx
+++ frontend/src/app/[movie]/page.tsx
@@ -4,17 +4,15 @@ export default function Movie({
   params: { movie: number };
 }) {
   return (
-    <div className="w-[1440px] h-[1024px] relative bg-white">
+    <div className="w-fit h-fit relative bg-black">
       <div className="w-[1288px] h-[807px] left-[76px] top-[137px] absolute">
         <div className="w-[1288px] h-[807px] left-0 top-0 absolute">
           <div className="w-[297px] h-[719px] left-0 top-0 absolute">
-            <div className="w-[135px] h-11 left-[17px] top-[675px] absolute">
-              <div className="w-[135px] h-11 left-0 top-0 absolute bg-orange-500 rounded-tl-[29px] rounded-bl-[29px]" />
-              <div className="w-[91px] h-6 left-[29px] top-[10px] absolute text-white text-[15px] font-bold font-['Maven Pro'] leading-normal">
+            <button className="w-[135px] h-11 left-[17px] top-[675px] rounded-tl-[29px] rounded-bl-[29px] bg-orange-500 absolute">
+              <p className="w-fit h-fit left-[29px] top-[10px] absolute text-white text-[15px] font-bold font-['Maven Pro'] leading-normal">
                 View Trailer
-                <br />
-              </div>
-            </div>
+              </p>
+            </button>
             <div className="w-[135px] h-11 left-[162px] top-[675px] absolute">
               <div className="w-[135px] h-11 left-0 top-0 absolute bg-orange-500 rounded-tr-[29px] rounded-br-[29px]" />
               <div className="w-[91px] h-6 left-[19px] top-[10px] absolute text-white text-[15px] font-bold font-['Maven Pro'] leading-normal">
@@ -22,12 +20,11 @@ export default function Movie({
                 <br />
               </div>
             </div>
-            <div className="w-[135px] h-11 left-0 top-0 absolute">
-              <div className="w-[135px] h-11 left-0 top-0 absolute bg-orange-500 rounded-tr-[29px] rounded-bl-[29px]" />
-              <div className="w-[91px] h-6 left-[34px] top-[10px] absolute text-white text-[15px] font-bold font-['Maven Pro'] leading-normal">
+            <button className="w-[135px] h-11 left-0 top-0 absolute bg-orange-500 rounded-tr-[29px] rounded-bl-[29px] flex flex-col justify-center" >
+              <p className="w-fit m-auto h-max left-[34px] top-[10px] text-white text-[15px] font-bold font-['Maven Pro'] leading-normal">
                 Go Back
-              </div>
-            </div>
+              </p>
+            </button>
           </div>
           <div className="w-[801px] h-[90px] left-[404px] top-[117px] absolute">
             <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
@@ -124,9 +121,6 @@ export default function Movie({
               <span className="text-orange-500 text-[32px] font-bold font-['Maven Pro'] leading-normal">
                 Reviews:
               </span>
-              <span className="text-white text-2xl font-normal font-['Maven Pro'] leading-normal">
-                  <br />
-              </span>
             </div>
           </div>
           <img
@@ -135,47 +129,8 @@ export default function Movie({
           />
         </div>
         <div className="left-[543px] top-[10px] absolute text-orange-500 text-5xl font-bold font-['Maven Pro'] leading-normal">
-          Movie #{movie}
+          {movie}
         </div>
-      </div>
-      <div className="w-[1470px] h-20 left-0 top-[944px] absolute">
-        <div className="w-[1470px] h-20 left-0 top-0 absolute bg-zinc-700 border border-black" />
-        <div className="w-[583px] h-[25.14px] left-[14px] top-[27.43px] absolute text-orange-500 text-xl font-bold font-['Inter']">
-          © 2024 Cinema E-Booking. All rights reserved.
-        </div>
-      </div>
-      <div className="w-[1440px] h-[95px] left-0 top-0 absolute">
-        <div className="w-[1440px] h-[84px] left-0 top-0 absolute bg-zinc-800 rounded-bl-[20px] rounded-br-[20px] shadow border-black" />
-        <div className="w-[60px] h-[60px] left-[1341px] top-[13px] absolute bg-zinc-800 bg-opacity-0" />
-        <div className="w-[751px] h-[83px] left-[281px] top-[12px] absolute">
-          <div className="w-[751px] h-[63px] left-0 top-0 absolute bg-stone-900 rounded-[29px]" />
-          <div className="w-36 h-[27px] left-[596px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Coming Soon
-          </div>
-          <div className="w-[35px] h-[0px] left-[575px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[175px] h-[30px] left-[382px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Currently Running
-          </div>
-          <div className="w-[35px] h-[0px] left-[361px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[154px] h-[63px] left-[224px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Order History
-          </div>
-          <div className="w-[35px] h-[0px] left-[202px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[97px] h-[63px] left-[130px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Profile
-          </div>
-          <div className="w-[35px] h-[0px] left-[109px] top-[16px] absolute origin-top-left rotate-90 border border-black"></div>
-          <div className="w-[94px] h-[63px] left-[36px] top-[20px] absolute text-white text-xl font-bold font-['Maven Pro'] leading-normal">
-            Home
-          </div>
-        </div>
-        <div className="w-[703px] h-[39px] left-[32px] top-[24px] absolute text-orange-500 text-[40px] font-extrabold font-['Maven Pro'] leading-normal">
-          NinjaFlix
-        </div>
-        <img
-          className="w-[54px] h-[49px] left-[200px] top-[14px] absolute"
-          src="https://via.placeholder.com/54x49"
-        />
       </div>
     </div>
   );
